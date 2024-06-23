d333901b5ec126397f8aea0fcb8d0703974a1a24
ldcruzl <lcruzl@unjbg.edu.pe>
2024-06-23 13:28:00 -0500

    Actualizacion ejercicio2: se cambio la estructura while a un for

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -7,12 +7,10 @@ int main(){
     int n1, n2, c, i;
     cout<<"ingrese dos numeros a comprobar: ";cin>>n1>>n2;
     c = 0;
-    i = 1;
-    while(i<=n1&&i<=n2){
+    for(i=1; i<=n1&&i<=n2; i++){
         if(n1%i==0&&n2%i==0){
             c++;
         }
-        i++;
     }
     if(c>1){
         cout<<"No son pesi"<<endl;
